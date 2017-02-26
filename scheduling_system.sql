8eddc856af630ecccc4cdeb3904c1b7acfcc233c
sanzillion <sanzimagery@gmail.com>
2017-02-26 12:08:29 +0800

    added new DB

--- scheduling_system.sql
+++ scheduling_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2017 at 03:52 AM
+-- Generation Time: Feb 26, 2017 at 04:07 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -47,22 +47,19 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 
 CREATE TABLE `subject` (
   `id` int(11) NOT NULL,
-  `teacher` int(11) NOT NULL,
-  `e4` int(11) NOT NULL,
-  `e3` int(11) NOT NULL,
-  `e2` int(11) NOT NULL,
-  `e1` int(11) NOT NULL
+  `teacher` varchar(100) NOT NULL,
+  `score` int(11) NOT NULL,
+  `students` int(11) NOT NULL,
+  `grade` varchar(3) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`id`, `teacher`, `e4`, `e3`, `e2`, `e1`) VALUES
-(16, 27, 5, 5, 13, 33),
-(18, 20, 16, 24, 8, 0),
-(17, 22, 37, 2, 1, 8),
-(19, 21, 5, 9, 2, 8);
+INSERT INTO `subject` (`id`, `teacher`, `score`, `students`, `grade`) VALUES
+(26, 'STING TARAZONA', 101, 4, '3.1'),
+(27, 'SHERYL RODEIGUEZ', 84, 4, '2.6');
 
 -- --------------------------------------------------------
 
@@ -82,8 +79,9 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`id`, `emp_id`, `name`, `department`) VALUES
-(20, '434347', 'SHERYL MAE RODRIGUEZ', 'ICT'),
-(21, '121211', 'MODESTO TARRAZONA', 'ICT');
+(28, '12345', 'STING TARAZONA', 'ICT'),
+(29, '54321', 'SHERYL RODEIGUEZ', 'ICT'),
+(30, '123ASD', 'RYAN PILAPIL', 'ICT');
 
 -- --------------------------------------------------------
 
@@ -119,9 +117,23 @@ CREATE TABLE `votes` (
   `id` int(11) NOT NULL,
   `code` varchar(100) NOT NULL,
   `teacher_name` varchar(100) NOT NULL,
-  `STRENGTHS` varchar(300) NOT NULL,
-  `WEAKNESSES` varchar(300) NOT NULL
+  `str` varchar(300) NOT NULL,
+  `weak` varchar(300) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `code`, `teacher_name`, `str`, `weak`) VALUES
+(12, 'MIsPysd', 'STING TARAZONA', 'asfafafafafafafafafa', 'fadasdasgagasdasd'),
+(10, 'RHiLwPv', 'STING TARAZONA', 'asdfasdfasdfasdf', 'asdfasdfasdfasdf'),
+(11, 'UfFxGWm', 'STING TARAZONA', 'asdfasdfasdfasdf', 'asdfadfasdfasdfasdf'),
+(13, 'YKLrm39', 'STING TARAZONA', 'asdfasdfasdfasdfasdf', 'asdfasdfasdfasdfasdfasdf'),
+(14, 'YKLrm39', 'SHERYL RODEIGUEZ', 'asdfasdfasdfasdfas', 'dfasdfasdfasdfasdf'),
+(15, 'UfFxGWm', 'SHERYL RODEIGUEZ', 'asdfasdfasdfasdf', 'asdfasfasdfasdfasdfadsf'),
+(16, 'RHiLwPv', 'SHERYL RODEIGUEZ', 'adsfasdfasdfasdf', 'asdfasdfadsfadsfasdf'),
+(17, 'MIsPysd', 'SHERYL RODEIGUEZ', '', '');
 
 --
 -- Indexes for dumped tables
@@ -170,12 +182,12 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `users`
 --
@@ -185,7 +197,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `votes`
 --
 ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
